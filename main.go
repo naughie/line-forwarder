867fe82a43d8d4ed76162d0696e1650c96566abb
Naughie <zomacchi@gmail.com>
2020-04-29 13:13:55 +0900

    Add logging

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "log"
     "os"
     "encoding/json"
     "strings"
@@ -109,21 +110,25 @@ func sendToIFTTT(userName, botName string) error {
 
 func forward(token Token, botName string) func(echo.Context) error {
     return func(c echo.Context) error {
+        log.Println("Handling request ...")
         obj := LINEObject{}
         if err := c.Bind(&obj); err != nil {
             return err
         }
         for _, e := range obj.Events {
             src := e.Source
+            log.Println("    Source: ", src)
             user, err := getUser(src.UserID, token)
             if err != nil {
                 return err
             }
+            log.Println("    DisplayName: ", user.DisplayName, ", BotName: ", botName)
             err = sendToIFTTT(user.DisplayName, botName)
             if err != nil {
                 return err
             }
         }
+        log.Println("Done")
         return c.NoContent(http.StatusOK)
     }
 }
@@ -131,6 +136,7 @@ func forward(token Token, botName string) func(echo.Context) error {
 func main() {
     token, err := fetchAccessToken()
     if err != nil {
+        log.Println("Cannot fetch access token")
         return
     }
 
